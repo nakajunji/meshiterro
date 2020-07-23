0ab6671c038fc8250aec24425f2051174f6280d1
vagrant <vagrant@localhost.localdomain>
2020-07-23 04:36:50 +0000

    [Start]5-6

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -24,7 +24,7 @@ class PostImagesController < ApplicationController
 	end
 
 	def destroy
-		@post_image = PostImage.find(paramas[:id])
+		@post_image = PostImage.find(params[:id])
 		@post_image.destroy
 		redirect_to post_images_path
 	end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
+  	@post_images = @user.post_images.page(params[:id]).reverse_order
+  end
+
+  def edit
+  	@user = User.find(params[:id])
   end
 end
